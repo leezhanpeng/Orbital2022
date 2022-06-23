9a6a0d0a60ea5d66ab3d6bd91f5834f19f719686
leezhanpeng <zhanpenglee@outlook.com>
2022-06-23 18:10:43 +0800

    adjusted homepage code

--- Client/src/Components/HomepageContent.js
+++ Client/src/Components/HomepageContent.js
@@ -7,7 +7,7 @@ import { useCookies }from 'react-cookie';
 
 const HomepageContent = () => {
   
-  const [news, setNews] = useState([]);
+  const [newsletter, setNews] = useState([]);
 
 
   useEffect(() => {
@@ -28,7 +28,7 @@ const HomepageContent = () => {
     return JSON.parse(window.atob(base64));
   }
 
-  const [cookies, setCookie] = useCookies();
+  const [cookies] = useCookies();
 
   const usernameDisplay = () => {
     return parseJwt(cookies.accesstoken).username;
@@ -40,71 +40,73 @@ const HomepageContent = () => {
 
 
         <div className={styles["welcomebox"]}>
-          <a className={styles["welcomeboxtext"]}>
+          <div className={styles["welcomeboxtext"]}>
             Hello, {usernameDisplay(0)}!
-          </a>
+          </div>
         </div>
 
 
         <div className={styles["newsbox"]}>
-          <a className={styles["newsheader"]}>
+          <div className={styles["newsheader"]}>
             NEWSLETTER
-          </a>
+          </div>
 
 
 
           {
-          news.map((news) => (
-          <div key={news.id} className={styles["news"]}>
-            <div><img src={news.imagebase64} className={styles["newsimg"]}></img></div>
+          newsletter.map((news, index) => (
+          <a href='/home' key={index} className={styles["news"]}>
+
+            <div ><img src={news.imagebase64} className={styles["newsimg"]} alt={"newletterimg"}></img></div>
 
             <div className={styles["newstext"]}>
 
-              <a className={styles["newstitle"]}>
+              <div className={styles["newstitle"]}>
                 {news.title}
-              </a>
-              <a className={styles["newscontent"]}>
+              </div>
+              <div className={styles["newscontent"]}>
                 {news.content}
-              </a>
+              </div>
 
             </div>
-          </div>
+
+          </a>
           ))
           }
 
 
 
-          <div key={"fakeid1"} className={styles["news"]}>
-            <div><img src={helloimg} className={styles["newsimg"]}></img></div>
+          <a href='/home' key={"fakeid1"} className={styles["news"]}>
+            <div><img src={helloimg} className={styles["newsimg"]} alt={"newletterimg"}></img></div>
 
             <div className={styles["newstext"]}>
 
-              <a className={styles["newstitle"]}>
+              <div className={styles["newstitle"]}>
                 Say hi to the developers!
-              </a>
-              <a className={styles["newscontent"]}>
+              </div>
+              <div className={styles["newscontent"]}>
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              </a>
+              </div>
 
             </div>
-          </div>
+          </a>
 
 
 
-          <div key={"fakeid2"} className={styles["news"]}>
-            <div><img src={welcomeimg} className={styles["newsimg"]}></img></div>
+          <a href='/home' key={"fakeid2"} className={styles["news"]}>
+            <div><img src={welcomeimg} className={styles["newsimg"]} alt={"newletterimg"}></img></div>
 
             <div className={styles["newstext"]}>
 
-              <a className={styles["newstitle"]}>
+              <div className={styles["newstitle"]}>
                 Welcome to PlayFab!
-              </a>
-              <a className={styles["newscontent"]}>
+              </div>
+              <div className={styles["newscontent"]}>
               Hello players! The website is currently in progress. Hope you are as excited as we are in the completion of this project!
-              </a>
+              </div>
 
             </div>
-          </div>
+          </a>
         </div>
 
       </div>
