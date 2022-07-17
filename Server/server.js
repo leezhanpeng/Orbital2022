fa4cddf4e5d001f9c83b4f490bee411901de694c
zhanpeng <zhanpenglee@outlook.com>
2022-07-17 17:34:20 +0800

    heroku

--- Server/server.js
+++ Server/server.js
@@ -281,12 +281,14 @@ app.post('/update-snake-records', async (req, res) => {
             SnakeRecord.updateOne({username: req.body.username}, {$set: {recordTime: timing, recordLength: setRecordLengthCleared, jewelsCollected: setJewels, sabosGiven: setSabos, powerupsReceived: setPower}}, (err, res) => {
             });
             res.status(200);
+            window.stop();
         }
         else
         {
             SnakeRecord.updateOne({username: req.body.username}, {$set: {recordLength: setRecordLengthCleared, jewelsCollected: setJewels, sabosGiven: setSabos, powerupsReceived: setPower}}, (err, res) => {
             });
             res.status(200);
+            window.stop();
         }
     }
     else
@@ -295,6 +297,7 @@ app.post('/update-snake-records', async (req, res) => {
         SnakeRecord.updateOne({username: req.body.username}, {$set: {snakeWins: setWins}}, (err, res) => {
         });
         res.status(200);
+        window.stop();
     }
 
 });
